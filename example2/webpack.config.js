18eb1a6a89a6b606871cd96a96df83bc31dcc674
Tomasz Lewinski <tomasz.lewinski@scalac.io>
2016-02-14 20:57:17 +0100

    typo

--- example2/webpack.config.js
+++ example2/webpack.config.js
@@ -1,12 +1,12 @@
 "use strict";
 
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     entry: ['./src/index.js', './src/style.css'],
     output: {
-        path: __dirname + "/dist",
+        path: __dirname + '/dist',
         filename: './bundle.js'
     },
     module: {
